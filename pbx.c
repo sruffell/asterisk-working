b4a730c91bbc3217b5e74062697161c48b8b7533
Mark Spencer <markster@digium.com>
2004-04-22 00:50:11 +0000

    Backport gethostbyname_r conversion
    git-svn-id: http://svn.asterisk.org/svn/asterisk/branches/v1-0_stable@2736 f38db490-d61c-443f-a65b-d21fe96a405b

--- pbx.c
+++ pbx.c
@@ -4348,7 +4348,7 @@ static int pbx_builtin_goto(struct ast_channel *chan, void *data)
 		ast_log(LOG_WARNING, "Goto requires an argument (optional context|optional extension|priority)\n");
 		return -1;
 	}
-	s = strdup((void *) data);
+	s = ast_strdupa((void *) data);
 	stringp=s;
 	context = strsep(&stringp, "|");
 	exten = strsep(&stringp, "|");
@@ -4368,7 +4368,6 @@ static int pbx_builtin_goto(struct ast_channel *chan, void *data)
 	}
 	if (atoi(pri) < 0) {
 		ast_log(LOG_WARNING, "Priority '%s' must be a number > 0\n", pri);
-		free(s);
 		return -1;
 	}
 	/* At this point we have a priority and maybe an extension and a context */
