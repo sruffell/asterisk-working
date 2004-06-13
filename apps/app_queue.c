27c21d070d692f66c47fa51049d6de591b5dc186
James Golovich <james@gnuinter.net>
2004-06-13 19:47:44 +0000

    Backport fix. Use strlen instead of &id in app_queue.c (Thanks folsson)
    git-svn-id: http://svn.asterisk.org/svn/asterisk/branches/v1-0_stable@3201 f38db490-d61c-443f-a65b-d21fe96a405b

--- apps/app_queue.c
+++ apps/app_queue.c
@@ -1471,7 +1471,7 @@ static int manager_queues_status( struct mansession *s, struct message *m )
 	time(&now);
 	ast_mutex_lock(&qlock);
 	q = queues;
-	if (id && &id) {
+	if (id && strlen(id)) {
 		snprintf(idText,256,"ActionID: %s\r\n",id);
 	}
 	while(q) {
