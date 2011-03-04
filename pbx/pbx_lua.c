sruffell/asterisk-working

commit bc0bd7e778cfe8191bf6023d56305c4204387e10

Add load priority order, such that preload becomes unnecessary in most cases
git-svn-id: http://svn.asterisk.org/svn/asterisk/trunk@278132 f38db490-d61c-443f-a65b-d21fe96a405b

pbx/pbx_lua.c
@@ -84,7 +84,7 @@ static void lua_create_application_metatable(lua_State *L);
 static void lua_create_autoservice_functions(lua_State *L);
 static void lua_create_hangup_function(lua_State *L);
 
-void lua_state_destroy(void *data);
+static void lua_state_destroy(void *data);
 static lua_State *lua_get_state(struct ast_channel *chan);
 
 static int exists(struct ast_channel *chan, const char *context, const char *exten, int priority, const char *callerid, const char *data);
@@ -108,7 +108,7 @@ static const struct ast_datastore_info lua_datastore = {
 /*!
  * \brief The destructor for lua_datastore
  */
-void lua_state_destroy(void *data)
+static void lua_state_destroy(void *data)
 {
 	if (data)
 		lua_close(data);
@@ -1467,7 +1467,7 @@ static int load_module(void)
 	return AST_MODULE_LOAD_SUCCESS;
 }
 
-AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_GLOBAL_SYMBOLS, "Lua PBX Switch",
+AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_DEFAULT, "Lua PBX Switch",
 		.load = load_module,
 		.unload = unload_module,
 		.reload = reload,

commit 0dd34826b46a436e364eabec64d8e3b89ca475fa

Merged revisions 309448 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.8
........
  r309448 | mnicholson | 2011-03-04 09:59:25 -0600 (Fri, 04 Mar 2011) | 8 lines
  
  Export global symbols from pbx_lua to allow modules to be loaded.  Fixes a regression introduced in r278132.
  
  (closes issue #18671)
  Reported by: Igels
  Patches:
        pbx_lua_global_symbols1.diff uploaded by mnicholson (license 96)
  Tested by: Igels
........


git-svn-id: http://svn.asterisk.org/svn/asterisk/trunk@309449 f38db490-d61c-443f-a65b-d21fe96a405b

pbx/pbx_lua.c
@@ -1467,7 +1467,7 @@ static int load_module(void)
 	return AST_MODULE_LOAD_SUCCESS;
 }
 
-AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_DEFAULT, "Lua PBX Switch",
+AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_GLOBAL_SYMBOLS, "Lua PBX Switch",
 		.load = load_module,
 		.unload = unload_module,
 		.reload = reload,