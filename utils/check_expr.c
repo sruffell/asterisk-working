sruffell/asterisk-working

commit 2946d45ee62368dc07d356d7ba1725d2bbb0f972

Fixed utils directory breakage from r398648
git-svn-id: http://svn.asterisk.org/svn/asterisk/branches/1.8@398748 f38db490-d61c-443f-a65b-d21fe96a405b

utils/check_expr.c
@@ -105,12 +105,19 @@ void ast_remove_lock_info(void *lock_addr)
 }
 #endif /* HAVE_BKTR */
 
+void ast_suspend_lock_info(void *lock_addr)
+{
+}
+void ast_restore_lock_info(void *lock_addr)
+{
+}
 void ast_mark_lock_acquired(void *);
 void ast_mark_lock_acquired(void *foo)
 {
     /* not a lot to do in a standalone w/o threading! */
 }
 #endif
+
 
 static int global_lineno = 1;
 static int global_expr_count=0;

utils/ael_main.c
@@ -647,5 +647,11 @@ void ast_store_lock_info(enum ast_lock_type type, const char *filename,
 {
 }
 #endif /* HAVE_BKTR */
+void ast_suspend_lock_info(void *lock_addr)
+{
+}
+void ast_restore_lock_info(void *lock_addr)
+{
+}
 #endif /* !defined(LOW_MEMORY) */
 #endif /* DEBUG_THREADS */

utils/conf2ael.c
@@ -777,5 +777,11 @@ void ast_store_lock_info(enum ast_lock_type type, const char *filename,
 {
 }
 #endif /* HAVE_BKTR */
+void ast_suspend_lock_info(void *lock_addr)
+{
+}
+void ast_restore_lock_info(void *lock_addr)
+{
+}
 #endif /* !defined(LOW_MEMORY) */
 #endif /* DEBUG_THREADS */

commit 0c6e1335234303fb7167a0b599ec81ac5ad76b56

Fixed utils directory breakage from r398748, this time with extra hate.
git-svn-id: http://svn.asterisk.org/svn/asterisk/branches/1.8@398752 f38db490-d61c-443f-a65b-d21fe96a405b

utils/check_expr.c
@@ -55,6 +55,7 @@ void * attribute_malloc _ast_calloc(size_t num, size_t len, const char *file, in
 }
 #endif
 
+#ifdef DEBUG_THREADS
 #if !defined(LOW_MEMORY)
 #ifdef HAVE_BKTR
 void ast_store_lock_info(enum ast_lock_type type, const char *filename,
@@ -117,6 +118,7 @@ void ast_mark_lock_acquired(void *foo)
     /* not a lot to do in a standalone w/o threading! */
 }
 #endif
+#endif /* DEBUG_THREADS */
 
 
 static int global_lineno = 1;