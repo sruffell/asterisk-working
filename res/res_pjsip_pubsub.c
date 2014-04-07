sruffell/asterisk-working

commit 0f5da552ad6ec77c5f3e381bc81cbd58e454932e

res_pjsip_pubsub: Add test event for state change
This adds a test event when subscription state changes so that
integration tests may trigger new actions at the appropriate times.

Review: https://reviewboard.asterisk.org/r/3383/
........

Merged revisions 411670 from http://svn.asterisk.org/svn/asterisk/branches/12


git-svn-id: http://svn.asterisk.org/svn/asterisk/trunk@411671 f38db490-d61c-443f-a65b-d21fe96a405b

res/res_pjsip_pubsub.c
@@ -42,6 +42,7 @@
 #include "asterisk/res_pjsip.h"
 #include "asterisk/callerid.h"
 #include "asterisk/manager.h"
+#include "asterisk/test.h"
 #include "res_pjsip/include/res_pjsip_private.h"
 
 /*** DOCUMENTATION
@@ -464,8 +465,18 @@ pjsip_dialog *ast_sip_subscription_get_dlg(struct ast_sip_subscription *sub)
 
 int ast_sip_subscription_send_request(struct ast_sip_subscription *sub, pjsip_tx_data *tdata)
 {
-	return pjsip_evsub_send_request(ast_sip_subscription_get_evsub(sub),
+	struct ast_sip_endpoint *endpoint = ast_sip_subscription_get_endpoint(sub);
+	int res = pjsip_evsub_send_request(ast_sip_subscription_get_evsub(sub),
 			tdata) == PJ_SUCCESS ? 0 : -1;
+	
+	ast_test_suite_event_notify("SUBSCRIPTION_STATE_SET",
+		"StateText: %s\r\n"
+		"Endpoint: %s\r\n",
+		pjsip_evsub_get_state_name(ast_sip_subscription_get_evsub(sub)),
+		ast_sorcery_object_get_id(endpoint));
+	
+	ao2_cleanup(endpoint);
+	return res;
 }
 
 static void subscription_datastore_destroy(void *obj)

commit 00cecc5fa79f1390d3eaae9694388432d57c7fa5

PJSIP: Fix crash introduced in r411671
The test event introduced in revision 411671 uses a dangling pointer to
access information about pubsub state changes. This moves the event to
within the lifetime of the pointer.


git-svn-id: http://svn.asterisk.org/svn/asterisk/branches/12@411790 f38db490-d61c-443f-a65b-d21fe96a405b

res/res_pjsip_pubsub.c
@@ -466,17 +466,16 @@ pjsip_dialog *ast_sip_subscription_get_dlg(struct ast_sip_subscription *sub)
 int ast_sip_subscription_send_request(struct ast_sip_subscription *sub, pjsip_tx_data *tdata)
 {
 	struct ast_sip_endpoint *endpoint = ast_sip_subscription_get_endpoint(sub);
-	int res = pjsip_evsub_send_request(ast_sip_subscription_get_evsub(sub),
-			tdata) == PJ_SUCCESS ? 0 : -1;
-	
+
 	ast_test_suite_event_notify("SUBSCRIPTION_STATE_SET",
 		"StateText: %s\r\n"
 		"Endpoint: %s\r\n",
 		pjsip_evsub_get_state_name(ast_sip_subscription_get_evsub(sub)),
 		ast_sorcery_object_get_id(endpoint));
-	
 	ao2_cleanup(endpoint);
-	return res;
+
+	return pjsip_evsub_send_request(ast_sip_subscription_get_evsub(sub),
+			tdata) == PJ_SUCCESS ? 0 : -1;
 }
 
 static void subscription_datastore_destroy(void *obj)